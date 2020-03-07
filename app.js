17273920b3e42d6fa85dad447c3bd36b361e8dbd
olliik <olli.isokoivisto@gmail.com>
2020-03-07 13:27:54 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const aedes = require('aedes')();
+const httpServer = require('http').createServer();
+const ws = require('websocket-stream');
+//const port = process.env.PORT || 8081;
+const port = 8081;
+
+ws.createServer({ server: httpServer }, aedes.handle );
+
+httpServer.listen(port, function () {
+    console.log('server started on PORT: ', port);
+});
